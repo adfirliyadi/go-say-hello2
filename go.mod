4e4317732f06caf4edd8da96fd37fe5e3d6d3962
adfirliyadi <firliyadi@gmail.com>
2021-09-25 23:42:04 +0700

    add parameter sayhello
    update go mod major upgrade

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/adfirliyadi/go-say-hello2
+module github.com/adfirliyadi/go-say-hello2/v2
 
 go 1.15
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello world tag v1.1.0 "
+func SayHello(name string) string {
+	return "Hello world tag v1.1.0 " + name
 }
